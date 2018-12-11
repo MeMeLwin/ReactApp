81c537cbcf7a40e06faecd162a14c70e1d0c081b
MeMeLwin <lwin.meme993@gmail.com>
2018-12-11 13:02:37 +0630

    react <form> testing

--- src/App.js
+++ src/App.js
@@ -1,32 +1,38 @@
 import React, { Component } from 'react';
 import './App.css';
 import Projects from './Components/Projects';
+import AddProject from './Components/AddProject';
 
 class App extends Component {
 
   constructor(){
     super();
     this.state={
-      projects:[
-        {
-          title:'Business',
-          category:'Web Design'
-        },
-        {
-          title:'Social App',
-          category:'Mobile Development'
-        },
-        {
-          title:'E-commerce Shopping Website',
-          category:'Web Development'
-        }
-      ]
+      projects:[]
+
     }
+  }
+
+  componentWillMount(){
+    this.setState({projects:[
+      {
+        title:'Business',
+        category:'Web Design'
+      },
+      {
+        title:'Social App',
+        category:'Mobile Development'
+      },
+      {
+        title:'E-commerce Shopping Website',
+        category:'Web Development'
+      }
+    ]});
   }
   render() {
     return (
       <div className="App">
-        My React App
+      <AddProject />
        <Projects projects={this.state.projects}/>
       </div>
     );
